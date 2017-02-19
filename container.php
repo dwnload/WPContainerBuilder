2fcde558f6ed8102415b9362bf8bc3b52fe43361
Austin Passy <austin@passy.co>
2017-02-18 18:42:24 -0800

    Update use misspelled class.

--- container.php
+++ container.php
@@ -9,19 +9,19 @@
  * License: MIT License
  */
 
-use Dwnload\WpContainerBuilder\BuilderFactory;
+use Dwnload\WpContainerBuilder\WpBuilderFactory;
 
 $factory = new WpBuilderFactory();
 add_action( 'plugins_loaded', function() use ( $factory ) {
 	$paths    = apply_filters( 'dwnload/wp_container_builder_paths', '', $factory );
 	$resource = apply_filters( 'dwnload/wp_container_builder_resource', '', $factory );
-	
+
 	$container = $factory->createContainer()->getContainerBuilder();
-	
+
 	/**
 	 * @var \Symfony\Component\DependencyInjection\ContainerBuilder $container
 	 */
 	do_action( 'dwnload/wp_container_builder_loaded', $container, $factory );
-	
+
 	$factory->loadServicesAndCompile( $paths, $resource );
 }, WpBuilderFactory::PRIORITY );
