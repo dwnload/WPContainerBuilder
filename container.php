868976fe29ba249a0257b1394038d16b7eb51ae6
Austin Passy <austin@passy.co>
2017-02-20 09:59:02 -0800

    Update filter and action docs.

--- container.php
+++ container.php
@@ -13,13 +13,32 @@ use Dwnload\WpContainerBuilder\WpBuilderFactory;
 
 $factory = new WpBuilderFactory();
 add_action( 'plugins_loaded', function() use ( $factory ) {
-	$paths    = apply_filters( 'dwnload/wp_container_builder_paths', '', $factory );
-	$resource = apply_filters( 'dwnload/wp_container_builder_resource', '', $factory );
+	/**
+	 * Apply paths filter array.
+	 *
+	 * @param array $paths
+	 * @param WpBuilderFactory $factory
+	 *
+	 * @return array
+	 */
+	$paths = apply_filters( 'dwnload/wp_container_builder_paths', [ __DIR__ ], $factory );
+	/**
+	 * Apply paths filter array.
+	 *
+	 * @param string $resource
+	 * @param WpBuilderFactory $factory
+	 *
+	 * @return string
+	 */
+	$resource = apply_filters( 'dwnload/wp_container_builder_resource', 'services.php', $factory );
 
 	$container = $factory->createContainer()->getContainerBuilder();
 
 	/**
-	 * @var \Symfony\Component\DependencyInjection\ContainerBuilder $container
+	 * Custom action when the Container Builder is loaded.
+	 *
+	 * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+	 * @param WpBuilderFactory $factory
 	 */
 	do_action( 'dwnload/wp_container_builder_loaded', $container, $factory );
 
